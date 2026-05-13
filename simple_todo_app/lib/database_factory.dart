501db9b05fbafe0f0bb99125f7b366d75e695fb4
宇田川 竜聖 <133090229+utaryu0520@users.noreply.github.com>
2026-05-13 14:47:36 +0900

    余分な書き込みによるエラー解消

--- simple_todo_app/lib/database_factory.dart
+++ simple_todo_app/lib/database_factory.dart
@@ -1,4 +1,4 @@
-http://localhost:8080import 'package:sembast/sembast.dart';
+//localhost:8080import 'package:sembast/sembast.dart';
 import 'database_factory_web.dart' if (dart.library.io) 'database_factory_io.dart';
 
-DatabaseFactory getDatabaseFactory() => databaseFactory();
+ getDatabaseFactory() => databaseFactory();
